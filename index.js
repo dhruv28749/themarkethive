5c1a756b1155ae866f9d5250171e10f6bce11233
dhruv28749 <147903068+dhruv28749@users.noreply.github.com>
2025-01-12 11:51:18 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function subscribe(){
+  let sub ={
+    email_id : document.getElementById("email_id").value.
+      }
+      emailjs.send("service_nic3tfc","template_pwqtriz",sub).then(alert("You have Subscribed to TheMarketHive"))
+
+}
+  
