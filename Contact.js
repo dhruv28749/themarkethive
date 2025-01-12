d5174eb767f3fdd0da3416b0e98f0a5600d37e78
dhruv28749 <147903068+dhruv28749@users.noreply.github.com>
2025-01-12 11:51:08 +0530

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -8,12 +8,3 @@ function sendMail(){
 
   emailjs.send("service_nic3tfc","template_wfffcks",parms).then(alert("Email Sent!"))
 }
-
-function subscribe(){
-  let sub ={
-    email_id : document.getElementById("email_id").value.
-      }
-      emailjs.send("service_nic3tfc","template_pwqtriz",sub).then(alert("You have Subscribed to TheMarketHive"))
-
-}
-  
