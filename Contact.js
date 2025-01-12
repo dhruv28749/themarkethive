b9072102ae4f3d56441bfd66e8ca7ad51ee4aedf
dhruv28749 <147903068+dhruv28749@users.noreply.github.com>
2025-01-12 11:03:10 +0530

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -1,4 +1,11 @@
 function sendMail(){
   let parms ={
     name : document.getElementById("name").value,
+    email : document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    message: document.getElementById("message").value,
+  }
+
+  emailjs("service_nic3tfc","template_wfffcks",parms).then(alert("Email SENT!"))
+}
     
